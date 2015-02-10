f084eb914cc913d8e0c7f29727ef332a62a8d90f
Carl Weis <appthentic@gmail.com>
2015-02-09 18:05:12 -0600

    added labels to post type generator

--- lib/PostType.php
+++ lib/PostType.php
@@ -67,10 +67,24 @@ class PostType
     public function register_post_type()
     {
         $n = ucwords($this->post_type_name);
-
+        $labels = array(
+            'name'                => _x( $this->pluralize($n) , 'text_domain' ),
+            'singular_name'       => _x( $this->singularize($n), 'text_domain' ),
+            'menu_name'           => __( $this->pluralize($n), 'text_domain' ),
+            'parent_item_colon'   => __( 'Parent ' . $this->singularize($n) . ':', 'text_domain' ),
+            'all_items'           => __( 'All ' . $this->pluralize($n), 'text_domain' ),
+            'view_item'           => __( 'View ' . $this->singularize($n), 'text_domain' ),
+            'add_new_item'        => __( 'Add New '. $this->singularize($n), 'text_domain' ),
+            'add_new'             => __( 'Add New ' . $this->singularize($n), 'text_domain' ),
+            'edit_item'           => __( 'Edit ' . $this->singularize($n), 'text_domain' ),
+            'update_item'         => __( 'Update '. $this->singularize($n), 'text_domain' ),
+            'search_items'        => __( 'Search '. $this->pluralize($n), 'text_domain' ),
+            'not_found'           => __( $this->singularize($n) . ' Not found', 'text_domain' ),
+            'not_found_in_trash'  => __( $this->singularize($n) . ' Not found in Trash', 'text_domain' ),
+        );
         $args = array(
-            "label" => $n . 's',
-            'singular_name' => $n,
+            "labels" => $labels,
+            'singular_name' => $this->singularize($n),
             "public" => true,
             "publicly_queryable" => true,
             "query_var" => true,
@@ -88,6 +102,140 @@ class PostType
 
         register_post_type($this->post_type_name, $args);
     }
+
+    /**
+    * Pluralizes English nouns.
+    *
+    * @access public
+    * @static
+    * @param    string    $word    English noun to pluralize
+    * @return string Plural noun
+    */
+    function pluralize($word)
+    {
+        $plural = array(
+        '/(quiz)$/i' => '1zes',
+        '/^(ox)$/i' => '1en',
+        '/([m|l])ouse$/i' => '1ice',
+        '/(matr|vert|ind)ix|ex$/i' => '1ices',
+        '/(x|ch|ss|sh)$/i' => '1es',
+        '/([^aeiouy]|qu)ies$/i' => '1y',
+        '/([^aeiouy]|qu)y$/i' => '1ies',
+        '/(hive)$/i' => '1s',
+        '/(?:([^f])fe|([lr])f)$/i' => '12ves',
+        '/sis$/i' => 'ses',
+        '/([ti])um$/i' => '1a',
+        '/(buffal|tomat)o$/i' => '1oes',
+        '/(bu)s$/i' => '1ses',
+        '/(alias|status)/i'=> '1es',
+        '/(octop|vir)us$/i'=> '1i',
+        '/(ax|test)is$/i'=> '1es',
+        '/s$/i'=> 's',
+        '/$/'=> 's');
+
+        $uncountable = array('equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep');
+
+        $irregular = array(
+        'person' => 'people',
+        'man' => 'men',
+        'child' => 'children',
+        'sex' => 'sexes',
+        'move' => 'moves');
+
+        $lowercased_word = strtolower($word);
+
+        foreach ($uncountable as $_uncountable){
+            if(substr($lowercased_word,(-1*strlen($_uncountable))) == $_uncountable){
+                return $word;
+            }
+        }
+
+        foreach ($irregular as $_plural=> $_singular){
+            if (preg_match('/('.$_plural.')$/i', $word, $arr)) {
+                return preg_replace('/('.$_plural.')$/i', substr($arr[0],0,1).substr($_singular,1), $word);
+            }
+        }
+
+        foreach ($plural as $rule => $replacement) {
+            if (preg_match($rule, $word)) {
+                return preg_replace($rule, $replacement, $word);
+            }
+        }
+        return false;
+
+    }
+
+    // }}}
+    // {{{ singularize()
+
+    /**
+    * Singularizes English nouns.
+    *
+    * @access public
+    * @static
+    * @param    string    $word    English noun to singularize
+    * @return string Singular noun.
+    */
+    function singularize($word)
+    {
+        $singular = array (
+        '/(quiz)zes$/i' => '\1',
+        '/(matr)ices$/i' => '\1ix',
+        '/(vert|ind)ices$/i' => '\1ex',
+        '/^(ox)en/i' => '\1',
+        '/(alias|status)es$/i' => '\1',
+        '/([octop|vir])i$/i' => '\1us',
+        '/(cris|ax|test)es$/i' => '\1is',
+        '/(shoe)s$/i' => '\1',
+        '/(o)es$/i' => '\1',
+        '/(bus)es$/i' => '\1',
+        '/([m|l])ice$/i' => '\1ouse',
+        '/(x|ch|ss|sh)es$/i' => '\1',
+        '/(m)ovies$/i' => '\1ovie',
+        '/(s)eries$/i' => '\1eries',
+        '/([^aeiouy]|qu)ies$/i' => '\1y',
+        '/([lr])ves$/i' => '\1f',
+        '/(tive)s$/i' => '\1',
+        '/(hive)s$/i' => '\1',
+        '/([^f])ves$/i' => '\1fe',
+        '/(^analy)ses$/i' => '\1sis',
+        '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\1\2sis',
+        '/([ti])a$/i' => '\1um',
+        '/(n)ews$/i' => '\1ews',
+        '/s$/i' => '',
+        );
+
+        $uncountable = array('equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep');
+
+        $irregular = array(
+        'person' => 'people',
+        'man' => 'men',
+        'child' => 'children',
+        'sex' => 'sexes',
+        'move' => 'moves');
+
+        $lowercased_word = strtolower($word);
+        foreach ($uncountable as $_uncountable){
+            if(substr($lowercased_word,(-1*strlen($_uncountable))) == $_uncountable){
+                return $word;
+            }
+        }
+
+        foreach ($irregular as $_plural=> $_singular){
+            if (preg_match('/('.$_singular.')$/i', $word, $arr)) {
+                return preg_replace('/('.$_singular.')$/i', substr($arr[0],0,1).substr($_plural,1), $word);
+            }
+        }
+
+        foreach ($singular as $rule => $replacement) {
+            if (preg_match($rule, $word)) {
+                return preg_replace($rule, $replacement, $word);
+            }
+        }
+
+        return $word;
+    }
+
 
     /**
      * Registers a new taxonomy, associated with the instantiated post type.
